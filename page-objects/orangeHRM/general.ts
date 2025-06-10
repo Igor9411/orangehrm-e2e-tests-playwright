c03daff7f32c716bdf1cebc43c50ac9141be16cd
Igor9411 <igorlitwin@o2.pl>
2025-06-10 20:22:00 +0200

    Latest updated of the login test

--- page-objects/orangeHRM/general.ts
+++ page-objects/orangeHRM/general.ts
@@ -1,6 +1,7 @@
 import {Page, Locator, expect} from '@playwright/test'
 
 export class LoginPage {
+
     readonly page: Page
 
     private usernameInput: Locator
@@ -9,28 +10,30 @@ export class LoginPage {
     private dashboard: Locator
 
     constructor(page:Page){
+
         this.page = page
 
         this.usernameInput = page.getByRole('textbox', { name: 'Username' })
         this.passwordInput = page.getByRole('textbox', { name: 'Password' })
         this.loginButton = page.getByRole('button', { name: 'Login' })
+        this.dashboard = page.getByRole('heading', { name: 'Dashboard' })
     }
     
     async goto(){
+
         await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     }
 
-    dashboardd = () => 
-            this.page.getByRole('heading', { name: 'Dashboard' })
-
     async login(username: string, password: string){
-    await this.usernameInput.fill(username)
-    await this.passwordInput.fill(password)
-    await this.loginButton.click()
+
+        await this.usernameInput.fill(username)
+        await this.passwordInput.fill(password)
+        await this.loginButton.click()
 
 }
     async dashboardVisibility(){
-        await expect(this.dashboardd()).toBeVisible()
+        
+        await expect(this.dashboard).toBeVisible()
     }
 
 }
--- tests/login-test.spec.ts
+++ tests/login-test.spec.ts
@@ -1,30 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../page-objects/orangeHRM/general.ts'
 
-test("Logging page", async ({page}) => {
-
-    await page.goto('')
-    
-    await expect(page).toHaveTitle('OrangeHRM')
-
-    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
-
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-
-    await page.getByRole('button', { name: 'Login' }).click()
-
-    await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible()
-});
-
 test("Login2", async ({ page }) => {
 
     const loginn = new LoginPage(page)
-    const dash = new LoginPage(page)
     
     await loginn.goto()
+
     await loginn.login("Admin", "admin123")
+
     await loginn.dashboardVisibility()
 
 })
